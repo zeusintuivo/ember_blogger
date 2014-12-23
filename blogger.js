089010ba3a5f6158e8633213e0689b4295dab950
zeusintuivo <zeus@intuivo.com>
2014-12-23 00:14:50 -0800

    Add property to controller

--- blogger.js
+++ blogger.js
@@ -1,3 +1,7 @@
 Blogger = Ember.Application.create({
   LOG_TRANSITIONS: true
 });
+
+// Blogger.ApplicationView = Ember.View.extend({
+//     templateName : 'templates/application'
+// });
---
+++ controllers/about.js
@@ -0,0 +1,7 @@
+Blogger.AboutController = Ember.Controller.extend({
+	actions: {
+		showRealName: function(){
+			alert("blah blah blah blah blah blah blah blah blah blah blah blah");
+		}
+	}
+});
