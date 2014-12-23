e40ec8e9b7e3016bd33a11791496257c0687dc5e
zeusintuivo <zeus@intuivo.com>
2014-12-23 07:07:49 -0800

    Add static model with a fixed javascript array.

---
+++ models/store.js
@@ -0,0 +1,38 @@
+var posts = [
+		{
+			id: 1,
+			title: "I love Woko Woko",
+			body: "Woko woko Woko woko Woko woko Woko woko Woko woko Woko woko Woko woko Woko woko Woko woko Woko woko Woko woko Woko woko Woko woko Woko woko Woko woko "
+		},
+
+		{
+			id: 2,
+			title: "I love Waka	 Waka	",
+			body: "Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 "
+		},
+
+		{
+			id: 3,
+			title: "I love Waku Waku",
+			body: "Waku waku Waku waku Waku waku Waku waku Waku waku Waku waku Waku waku Waku waku Waku waku Waku waku Waku waku Waku waku Waku waku Waku waku Waku waku "
+		},
+];
+var comments = [
+		{
+			id: 1,
+			title: "I think that Woko Woko",
+			body: "Woko woko Woko woko Woko woko Woko woko Woko woko Woko woko Woko woko Woko woko Woko woko Woko woko Woko woko Woko woko Woko woko Woko woko Woko woko "
+		},
+
+		{
+			id: 2,
+			title: "I believe that Waka	 Waka	",
+			body: "Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 Waka	 waka	 "
+		},
+
+		{
+			id: 3,
+			title: "I disagree with Waku Waku",
+			body: "Waku waku Waku waku Waku waku Waku waku Waku waku Waku waku Waku waku Waku waku Waku waku Waku waku Waku waku Waku waku Waku waku Waku waku Waku waku "
+		},
+];
