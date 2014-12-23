06265ab723fc034366820fb36938b5421e72cfb9
zeusintuivo <zeus@intuivo.com>
2014-12-23 00:28:20 -0800

    Move action to actions. Property and action are different.

--- controllers/about.js
+++ controllers/about.js
@@ -2,6 +2,12 @@ Blogger.AboutController = Ember.Controller.extend({
 	actions: {
 		showRealName: function(){
 			alert("blah blah blah blah blah blah blah blah blah blah blah blah");
+		},
+		showPicture: function() {
+			this.set('isPictureShowing', true);
 		}
-	}
+	},
+	isPictureShowing: false,
+
+
 });
